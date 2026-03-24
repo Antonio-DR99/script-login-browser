969b310b9c3435c0c6011217979cbc30649ee9cc
Antonio <adelarosariquelme1999@gmail.com>
2026-03-24 16:41:36 +0100

    Añadir script de correo y contraseña con readline-sync

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Todo funcionando 🚀");
+import readlineSync from "readline-sync";
+
+const email = readlineSync.question("Introduce tu correo: ");
+const password = readlineSync.question("Introduce tu contraseña: ", { hideEchoBack: true });
+
+console.log("\nTus datos son:");
+console.log("Correo:", email);
+console.log("Contraseña:", password); 
